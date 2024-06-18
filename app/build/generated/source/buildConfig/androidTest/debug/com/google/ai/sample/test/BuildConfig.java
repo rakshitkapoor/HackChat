e2f534ce569c8ba26051742ee36124133ef0db6d
rakshitkapoor <rakshitkapoor1305@gmail.com>
2024-06-18 18:29:58 +0530

    upload_comit

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/google/ai/sample/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.google.ai.sample.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.google.ai.sample.test";
+  public static final String BUILD_TYPE = "debug";
+}
---
+++ app/src/main/kotlin/com/google/ai/sample/GenerativeAiViewModelFactory.kt
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.ai.sample
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.google.ai.client.generativeai.GenerativeModel
+import com.google.ai.client.generativeai.type.BlockThreshold
+import com.google.ai.client.generativeai.type.HarmCategory
+import com.google.ai.client.generativeai.type.SafetySetting
+import com.google.ai.client.generativeai.type.generationConfig
+import com.google.ai.sample.feature.chat.ChatViewModel
+
+
+val GenerativeViewModelFactory = object : ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(
+        viewModelClass: Class<T>,
+        extras: CreationExtras
+    ): T {
+        val config = generationConfig {
+            temperature = 0.9f
+            topK = 1
+            topP = 1f
+            maxOutputTokens = 2048
+        }
+
+        return with(viewModelClass) {
+            when {
+
+                isAssignableFrom(ChatViewModel::class.java) -> {
+                    // Initialize a GenerativeModel with the `gemini-pro` AI model for chat
+                    val generativeModel = GenerativeModel(
+                        modelName = "gemini-pro",
+                        apiKey = BuildConfig.apiKey,
+                        generationConfig = config,
+                        safetySettings = listOf(
+                            SafetySetting(HarmCategory.HARASSMENT, BlockThreshold.MEDIUM_AND_ABOVE),
+                        SafetySetting(HarmCategory.HATE_SPEECH, BlockThreshold.MEDIUM_AND_ABOVE),
+                        SafetySetting(HarmCategory.SEXUALLY_EXPLICIT, BlockThreshold.MEDIUM_AND_ABOVE),
+                        SafetySetting(HarmCategory.DANGEROUS_CONTENT, BlockThreshold.MEDIUM_AND_ABOVE),
+                    ),
+
+                    )
+                    ChatViewModel(generativeModel)
+                }
+
+                else ->
+                    throw IllegalArgumentException("Unknown ViewModel class: ${viewModelClass.name}")
+            }
+        } as T
+    }
+}
